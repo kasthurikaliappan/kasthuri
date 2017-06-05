20a0130759ccc89e0d09eefbb296ca5fd5d0759b
kasthurikaliappan <kasthuridivya97@gmail.com>
2017-06-05 12:10:40 +0530

    Add files via upload

---
+++ kk.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+voidmain()
+{
+  int a b;
+  printf("enter the value");
+  scanf("%",&a);
+  if(a>0)
+  printf("the value is positive");
+  else if(a<0)
+  printf("the value is negative");
+  else
+  printf("the value is zero");
+   }
